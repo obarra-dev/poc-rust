7bce91f4e3f855f271db28950bfc8e0737d59aef
obarra-dev <barra_omar@hotmail.com>
2024-10-07 00:28:11 -0300

    more examples

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,9 +2,12 @@ mod operation;
 use std::io;
 
 fn main() {
-     println!("Hello, world omar!");
+    println!("Hello, world omar!");
+    println!("z is {:?}", 4);
 
-     let mut input = String::new();
-     io::stdin().read_line(&mut input).expect("faild to read line");
-     print!("{}", input);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("faild to read line");
+    print!("{}", input);
 }
--- some-tests/src/lib.rs
+++ some-tests/src/lib.rs
@@ -4,7 +4,7 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use std::i32;
+    use std::{fmt::format, i32, ops::RangeInclusive};
 
     use super::*;
 
@@ -26,7 +26,8 @@ mod tests {
         y = 10;
         assert_eq!(y, 10);
 
-        // rust allows to overrides variables
+        // rust allows to overrides variables, it is named shadowing
+        // the first variable is shadowed by the second one
         let z = 4;
         assert_eq!(z, 4);
         let z = 20;
@@ -37,6 +38,10 @@ mod tests {
         assert_eq!(a, 4);
         let a = "omar";
         assert_eq!(a, "omar");
+
+        // underscore is to prevent warnings, #[allow(unused_variables)] also can be used
+        let _v = 4;
+        assert_eq!(_v, 4);
     }
 
     #[test]
@@ -45,7 +50,7 @@ mod tests {
         assert_eq!(x, 4);
 
         {
-            // interior scope, this x valuse only lives here
+            // interior SCOPE, this x valuse only lives here
             let x = x - 2;
             assert_eq!(x, 2);
         }
@@ -74,6 +79,59 @@ mod tests {
         assert_eq!(tuple.2, 5);
         assert_eq!(tuple.1, false);
         assert_eq!(tuple.0, 'd');
+
+        // tuple from 1 to 4 inclusive
+        assert_eq!((1..=4), RangeInclusive::new(1, 4));
+    }
+
+    #[test]
+    fn size_of_val() {
+        let c = 'o';
+        let s = std::mem::size_of_val(&c);
+        assert_eq!(s, 4);
+
+        let c = '🙀';
+        let s = std::mem::size_of_val(&c);
+        assert_eq!(s, 4);
+    }
+
+    #[test]
+    fn unit_type() {
+        fn implicitly_ret_unit() {};
+        fn explicitly_ret_unit() -> () {};
+
+        // unit represents empty tuple of 0 bytes
+        // used to return  nothing in expressions or functions
+        let unit = ();
+        let s = std::mem::size_of_val(&unit);
+        assert_eq!(s, 0);
+        assert_eq!(unit, ());
+        assert_eq!(explicitly_ret_unit(), ());
+        assert_eq!(implicitly_ret_unit(), ());
+        assert_eq!(explicitly_ret_unit(), implicitly_ret_unit());
+    }
+
+    #[test]
+    fn tuple_destructuring() {
+        let (x, y, z) = ('o', true, 4);
+        assert_eq!(z, 4);
+        assert_eq!(y, true);
+        assert_eq!(x, 'o');
+
+        let (mut x, y, z) = ('o', true, 4);
+        x = 'd';
+        assert_eq!(z, 4);
+        assert_eq!(y, true);
+        assert_eq!(x, 'd');
+
+        // Destructuring assignments
+        // one line instead of 2 lines
+        let (a, b);
+        // let is no neded, destructuring the first one
+        (a, ..) = (3, 4);
+        // destructuring the second one
+        [.., b] = [1, 2];
+        assert_eq!([a, b], [3, 2]);
     }
 
     #[test]
@@ -94,6 +152,14 @@ mod tests {
 
     #[test]
     fn arithmetic_type_casting() {
+        // 1024 + 255 + 63 + 255
+        // perform math operation on diff number systemdd
+        let v = 1_024 + 0xFF + 0o77 + 0b1111_1111;
+        assert_eq!(v, 1597);
+
+        let v: u16 = 38_u8 as u16;
+        assert_eq!(v, 38);
+
         // the result of any arithmetic operation we perform has to be always the same type as the two operators
         let x = (i32::MAX as i64) + 1;
         let y = 10_i32;
@@ -108,6 +174,44 @@ mod tests {
         let input = "8";
         let int_input: i64 = input.trim().parse().unwrap();
         assert_eq!(int_input, 8);
+
+        let value: u16 = 5;
+        let value_type = type_of(&value);
+        assert_eq!(value_type, "u16");
+
+        // ASCII
+        assert_eq!('o' as u8, 111);
+    }
+
+    // TODO to check
+    fn type_of<T>(_: &T) -> String {
+        format!("{}", std::any::type_name::<T>()) //
+    }
+
+    #[test]
+    fn checked_add() {
+        // both would overflow, they do not compile
+        // 251_u8 + 8;
+        // i8::checked_add(251, 8).unwrap();
+
+        let v = 251_u16 + 8;
+        assert_eq!(v, 259);
+
+        // this is safer
+        let v = i16::checked_add(251, 8).unwrap();
+        assert_eq!(v, 259);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed: 0.1 + 0.2 == 0.3")]
+    fn arithmetic_overflow() {
+        // it is 0.3333...
+        assert!(0.1 + 0.2 == 0.3)
+    }
+
+    #[test]
+    fn arithmetic_overflow_fixed() {
+        assert!(0.1_f32 + 0.2_f32 == 0.3_f32)
     }
 
     #[test]
@@ -131,8 +235,14 @@ mod tests {
             let x = 3;
             x + 1
         };
-
         assert_eq!(a, 4);
+
+        // semicolon suppresses this expression, so it returns unit type, nothing
+        let b = {
+            let x = 3;
+            x + 1;
+        };
+        assert_eq!(b, ());
 
         let b = my_function(4);
         assert_eq!(b, 14)
@@ -148,5 +258,34 @@ mod tests {
         }
 
         0
+    }
+
+    #[test]
+    fn string_test() {
+        let a = String::from("omar");
+        assert_eq!(a, "omar");
+    }
+
+    #[test]
+    fn for_test() {
+        let mut sum = 0;
+        // from 3 to 2, 2 is not included
+        for i in -3..2 {
+            sum += i;
+        }
+        assert_eq!(sum, -5);
+
+        // from 3 to 2, 2 is  included
+        let mut sum = 0;
+        for i in -3..=2 {
+            sum += i;
+        }
+        assert_eq!(sum, -3);
+
+        let mut sum = 0;
+        for i in 'a'..='c' {
+            sum += i as i32;
+        }
+        assert_eq!(sum, 294);
     }
 }
