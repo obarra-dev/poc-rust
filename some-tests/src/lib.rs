85953c03face1f18a25269a62dd7aa0e8f64bf71
obarra-dev <barra_omar@hotmail.com>
2024-10-20 18:13:26 -0300

    control flow

--- some-tests/src/lib.rs
+++ some-tests/src/lib.rs
@@ -347,29 +347,6 @@ mod tests {
     }
 
     #[test]
-    fn for_test() {
-        let mut sum = 0;
-        // from 3 to 2, 2 is not included
-        for i in -3..2 {
-            sum += i;
-        }
-        assert_eq!(sum, -5);
-
-        // from 3 to 2, 2 is  included
-        let mut sum = 0;
-        for i in -3..=2 {
-            sum += i;
-        }
-        assert_eq!(sum, -3);
-
-        let mut sum = 0;
-        for i in 'a'..='c' {
-            sum += i as i32;
-        }
-        assert_eq!(sum, 294);
-    }
-
-    #[test]
     fn ownership() {
         // owner and other_owner are saved on stack
         let owner = 4;
@@ -1035,5 +1012,203 @@ mod tests {
                 _ => panic!("NEVER LET THIS RUN"),
             }
         }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    #[test]
+    fn enum_option() {
+        let five = Some(5);
+        let six = plus_one(five);
+        assert_eq!(six.unwrap(), 6);
+        // othe way to get the value, destructuring
+        if let Some(i) = six {
+            assert_eq!(i, 6);
+        }
+
+        let x = plus_one(None);
+        assert_eq!(x, None);
+        // it will panic
+        // assert_eq!(x.unwrap(), 4);
+
+        let some_number = Some(5);
+        let some_string = Some("a string");
+        let absent_number: Option<i32> = None;
+        assert_eq!(some_number, Some(5));
+        assert_eq!(some_string, Some("a string"));
+        assert_eq!(absent_number, None);
+
+        let x: i8 = 5;
+        let y: Option<i8> = Some(5);
+        let sum = x + y.unwrap();
+        assert_eq!(sum, 10);
+
+        let x: i8 = 5;
+        let y: Option<i8> = Some(5);
+        let sum = x + y.unwrap_or(0);
+        assert_eq!(sum, 10);
+
+        let x: i8 = 5;
+        let y: Option<i8> = None;
+        let sum = x + y.unwrap_or(0);
+        assert_eq!(sum, 5);
+
+        let x: i8 = 5;
+        let y: Option<i8> = None;
+        // does not compile, error: the `?` operator can only be used in a function that returns `Result` or `Option`
+        // let sum = x + y?;
+        // assert_eq!(sum, 5);
+
+        let x: i8 = 5;
+        let y: Option<i8> = Some(5);
+        // let sum = x + y?;
+        // assert_eq!(sum, 10);
+    }
+
+    #[test]
+    #[should_panic(expected = "called `Option::unwrap()` on a `None` value")]
+    fn enum_option_panic() {
+        let x = plus_one(None);
+        assert_eq!(x.unwrap(), 4);
+    }
+
+    #[test]
+    fn if_test() {
+        // if/else expression can be used in assignment
+        let number = if true { 5 } else { 6 };
+        assert!(number == 5);
+    }
+
+    #[test]
+    fn for_test() {
+        let mut sum = 0;
+        for i in 0..5 {
+            if i == 1 {
+                continue;
+            }
+            if i == 3 {
+                break;
+            }
+            sum += i;
+        }
+        assert_eq!(sum, 2);
+
+        let mut sum = 0;
+        // from 3 to 2, 2 is not included
+        for i in -3..2 {
+            sum += i;
+        }
+        assert_eq!(sum, -5);
+
+        // from 3 to 2, 2 is  included
+        let mut sum = 0;
+        for i in -3..=2 {
+            sum += i;
+        }
+        assert_eq!(sum, -3);
+
+        let mut sum = 0;
+        for i in 'a'..='c' {
+            sum += i as i32;
+        }
+        assert_eq!(sum, 294);
+
+        let names = [String::from("omar"), String::from("barra")];
+        assert_eq!(names.len(), 2);
+        for name in names {
+            assert!(name == "omar" || name == "barra");
+        }
+        // does not compile, the for tool the ownership of the values in the array
+        // assert_eq!(names.len(), 2);
+
+        let names = [String::from("omar"), String::from("barra")];
+        // to avoid taking the ownership, we can use iter()
+        for name in names.iter() {
+            assert!(name == "omar" || name == "barra");
+        }
+        assert_eq!(names.len(), 2);
+
+        // other way: to avoid taking the ownership, we can use &
+        for name in &names {
+            assert!(name == "omar" || name == "barra");
+        }
+        assert_eq!(names.len(), 2);
+
+        let a = [3, 2, 3];
+
+        let mut index: usize = 0;
+        let mut sum = 0;
+        // enumerate returns a tuple with the index and the value
+        for (i, v) in a.iter().enumerate() {
+            index = index + i + 1;
+            sum = sum + v;
+        }
+        assert_eq!(index, 6);
+        assert_eq!(sum, 8);
+    }
+
+    #[test]
+    fn while_test() {
+        let mut index: u8 = 0;
+        while index < 10 {
+            index += 1;
+        }
+        assert_eq!(index, 10);
+    }
+
+    #[test]
+    fn loop_test() {
+        let mut count: u8 = 0;
+
+        // infinite loop, usually used together with break and continue
+        loop {
+            count += 1;
+
+            if count == 3 || count == 7 {
+                continue;
+            }
+
+            if count == 10 {
+                break;
+            }
+        }
+        assert_eq!(count, 10);
+
+        // loop is an expression, so it can return a value
+        let mut count: u8 = 0;
+        let r = loop {
+            count += 1;
+            if count == 10 {
+                break count + 1;
+            }
+        };
+        assert_eq!(r, 11);
+
+        // Nesting and labels with loop
+        let mut count = 0;
+        'outer: loop {
+            'inner1: loop {
+                if count >= 20 {
+                    break 'inner1;
+                }
+                count += 2;
+            }
+
+            count += 5;
+
+            'inner2: loop {
+                if count >= 30 {
+                    break 'outer;
+                }
+
+                continue 'outer;
+            }
+        }
+        assert_eq!(count, 30);
     }
 }
