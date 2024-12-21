c5d2d9f2a98e70eb9248728f0dd99ae66a547813
obarra <obarra@obarras-MacBook-Pro.local>
2024-12-21 09:28:45 -0300

    rust lifetiems

--- some-tests/tests/test_lifetime.rs
+++ some-tests/tests/test_lifetime.rs
@@ -1,11 +1,34 @@
 #[test]
-fn read_file() {
-    // using pattern matching
-    let s = read_file_by_pattern_matching();
-    // using ? operator
-    let s1 = read_file_by_question_mark_op();
-    let result = s.unwrap_err().to_string();
-    assert_eq!(result, s1.unwrap_err().to_string());
-    assert_eq!(result, "The system cannot find the file specified. (os error 2)");
+fn  danglin_reference() {
+    // lifetime of r is valid until the end of the test
+    let r;
+    {
+        // life time of x is valid until the end of this scope
+        let x = 1;
+        r = &x;
+    }
+    
+    // this does not compile, error `x` does not live long enough
+    // since r points to something that is not valid anymore, dangling pointer/references
+    // println!("r: {}", r);
+}
+
+#[test]
+fn values_lives_on_its_scope() {
+    // v has the longest lifetime because its scope enterily encloses both borrow and borrow2
+    let v = 4;
+    {
+        let borrow_r = &v;
+        // TODO why it does not need to dereference
+        println!("borrow_r: {}", borrow_r);
+        // TODO a better way to do it?
+        assert_eq!(*borrow_r, 4);
+   }
+    
+   {
+    let borrow_r2 = &v;
+    assert_eq!(*borrow_r2, 4);
+}
+
 
 }
