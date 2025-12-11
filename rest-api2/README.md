4fc5cd4e55700cffb9144d1cab2e61ee81f471c1
obarra-dev <obarra@sonatype.com>
2025-12-10 21:05:42 -0300

    api rest

---
+++ rest-api2/README.md
@@ -0,0 +1,7 @@
+cargo --version
+rustc --version
+rustup update
+rustup self uninstall
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cargo add serde
---
+++ rest-api2/src/main.rs
@@ -0,0 +1,110 @@
+use postgres::{ Client, NoTls};
+use postgres::Error as PostgresError;
+use std::net::{TcpListener, TcpStream};
+use std::io::{ Read, Write};
+use std::env;
+use serde::de::Error;
+// TODO if i use macro use it is unecessary why?
+// use serde_derive::{Deserialize, Serialize};
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+// TODO how to use env?
+//const DB_URL: &str = env!("DATABASE_URL");
+const DB_URL: &str = "postgres://postgres:password@localhost:5432/for-rust";
+//const DB_URL: &str = "host=localhost user=postgres password=password";
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND";
+const INTERNAL_SERVER_RESPONSE: &str = "HTTP/1.1 500 Internal Server Error";
+
+
+fn main() {
+    if let Err(e) = set_database() {
+        println!("Failed to set database: {}", e);
+        return;
+    }
+
+    //let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    // TODO why format? why 00000?
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+                println!("OK");
+            }
+            Err(e) => {
+                print!("Error: {}", e);
+            }
+        }
+    }
+
+    println!("Omar rules!");
+}
+
+fn set_database() -> Result<(), PostgresError> {
+    // TODO how to return the error
+
+    //  TODO why symbol ?
+    let mut client = Client::connect(DB_URL, NoTls)?;
+
+    client.batch_execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )")?;
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            // TODO what??
+            request.push_str(&String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            let (status_line, content) = match &*request {
+                r if r.starts_with("POST /users") => handle_post_request(r),
+                _ => (NOT_FOUND_RESPONSE.to_string(), "404 not found".to_string()), // TODO to_string is necessar?
+            };
+
+            stream.write_all(format!("{}\n{}", status_line, content).as_bytes()).unwrap();
+        }
+        Err(e) => {
+            println!("Failed to read from stream: {}", e);
+        }, // TODO why coma?
+    }
+}
+
+fn handle_post_request(request: &str) -> (String, String) {
+    match (get_user_request_body(&request), Client::connect(DB_URL, NoTls)) {
+        (Ok(user), Ok(mut client)) => {
+            client
+                .execute(
+                    "INSERT INTO users (name, email) VALUES ($1, $2)",
+                    &[&user.name, &user.email]
+                )
+                .unwrap();
+
+            (OK_RESPONSE.to_string(), "User created".to_string())
+        }
+        _ => (INTERNAL_SERVER_RESPONSE.to_string(), "Error".to_string()),
+    }
+}
+
+
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    // TODOwhy this string?
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
