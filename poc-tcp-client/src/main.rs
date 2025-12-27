a1960494a1895c2daca504ea1284a3dfc618f1c7
obarra-dev <obarra@sonatype.com>
2025-12-27 11:18:39 -0300

    client

--- poc-tcp-client/src/main.rs
+++ poc-tcp-client/src/main.rs
@@ -1,17 +1,67 @@
-use std::io::Write;
+use std::env;
+use std::process;
+use std::thread;
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
 
+type Port = u16;
+
 fn main() {
-    run_client("localhost", &7878).unwrap();
-    println!("Message sent!");
-}
+    let mut args = env::args();
+    let program_name = args.next().unwrap_or("test".to_string());
 
-fn run_client(host: &str, port: &u16) -> Result<(), String> {
-    let addr = format!("{}:{}", host, port);
-    let mut client = TcpStream::connect(addr.as_str())
-        .map_err(|_| format!("Failed to connect to {}", addr))?;
+    let host = args.next().unwrap_or_else(|| {
+        println!("usage: {} HOST PORT", program_name);
+        process::exit(-1);
+    });
 
-    client.write(b"Omar rules from client")
-        .map_err(|_| format!("Failed to write to client"))?;
-    Ok(())
+    let port = args.next().unwrap_or_else(|| {
+        println!("usage: {} HOST PORT", program_name);
+        process::exit(-1);
+    }).parse::<Port>().unwrap_or_else(|error| {
+        writeln!(io::stderr(),"{}: error: {}", program_name, format!("invalid port number: {}",error));
+        println!("usage: {} HOST PORT", program_name);
+        process::exit(-1);
+    });
+
+    let mut stream = TcpStream::connect((host.as_str(), port))
+        .unwrap_or_else(|error| {
+            writeln!(io::stderr(),"{}: error: {}", program_name, error);
+            process::exit(-1);
+        });
+
+    let mut input_stream = stream.try_clone().unwrap();
+
+    let handler = thread::spawn(move || {
+        let mut client_buffer = [0u8; 1024];
+
+        loop {
+            match input_stream.read(&mut client_buffer) {
+                Ok(n) => {
+                    if n == 0 {
+                        process::exit(0);
+                    }
+                    else
+                    {
+                        io::stdout().write(&client_buffer).unwrap();
+                        io::stdout().flush().unwrap();
+                    }
+                },
+                Err(error) => {
+                    writeln!(io::stderr(),"{}: error: {}", program_name, error);
+                    process::exit(-1);
+                },
+            }
+        }
+    });
+
+    let output_stream = &mut stream;
+    let mut user_buffer = String::new();
+
+    loop {
+        io::stdin().read_line(&mut user_buffer).unwrap();
+
+        output_stream.write(user_buffer.as_bytes()).unwrap();
+        output_stream.flush().unwrap();
+    }
 }
