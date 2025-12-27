646f6d8d328b26e62723f6542c38a9907423b5d6
obarra-dev <obarra@sonatype.com>
2025-12-27 12:00:47 -0300

    client

--- poc-tcp-client/src/main.rs
+++ poc-tcp-client/src/main.rs
@@ -1,17 +1,26 @@
+use std::io;
 use std::io::Write;
 use std::net::TcpStream;
 
-fn main() {
-    run_client("localhost", &7878).unwrap();
-    println!("Message sent!");
-}
-
-fn run_client(host: &str, port: &u16) -> Result<(), String> {
-    let addr = format!("{}:{}", host, port);
-    let mut client = TcpStream::connect(addr.as_str())
+fn main() -> Result<(), String> {
+    let addr = format!("{}:{}", "localhost", 7878);
+    let mut client_stream = TcpStream::connect(addr.as_str())
         .map_err(|_| format!("Failed to connect to {}", addr))?;
 
-    client.write(b"Omar rules from client")
+    client_stream.write(b"Initial message")
         .map_err(|_| format!("Failed to write to client: {}", addr))?;
+
+    let mut user_buffer = String::new();
+    loop {
+        io::stdin().read_line(&mut user_buffer).unwrap();
+        if "END\n" == user_buffer {
+            break;
+        }
+        client_stream.write(user_buffer.as_bytes()).unwrap();
+        client_stream.flush().unwrap();
+        user_buffer.clear();
+    }
+
+    println!("Omar rules!");
     Ok(())
 }
