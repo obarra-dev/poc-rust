4cb42f15ce62476a824df9594dd06206b15ee475
obarra-dev <obarra@sonatype.com>
2025-12-28 18:58:18 -0300

    print files

--- poc-ls/src/main.rs
+++ poc-ls/src/main.rs
@@ -1,13 +1,28 @@
 use std::fs;
-use std::fs::metadata;
+use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
 use clap::Parser;
 use owo_colors::OwoColorize;
+use tabled::Tabled;
 
 #[derive(Parser, Debug)]
 #[command(version)]
 struct CLI {
     path: Option<PathBuf>,
+}
+
+#[derive(Debug)]
+enum EntryType {
+    File,
+    Directory,
+}
+
+#[derive(Debug, Tabled)]
+struct FileEntry {
+    name : String,
+    e_type : EntryType,
+    len_byte: u64,
+    modified: String,
 }
 
 fn main() {
@@ -23,7 +38,7 @@ fn main() {
 
         println!("{} exists", path.display());
         for file in get_files(&path) {
-            println!("{}", file);
+            println!("{:?}", file);
         }
     } else {
         eprintln!("{}", "Error reading path.".red());
@@ -32,18 +47,33 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn get_files(path: &Path) -> Vec<String> {
+fn get_files(path: &Path) -> Vec<FileEntry> {
     let mut files = Vec::default();
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             // TODO what can I do with error?
             if let Ok(entry) = entry {
-                files.push(entry.file_name()
-                    .into_string()
-                    .unwrap_or("unknown file".to_string()));
+                add_file(entry, &mut files);
             }
         }
     }
 
     files
+}
+
+fn add_file(file_entry: DirEntry, files: &mut Vec<FileEntry>) {
+    if let Ok(metadata) = fs::metadata(&file_entry.path()) {
+        files.push(FileEntry{
+            name: file_entry.file_name().into_string()
+                .unwrap_or("unknown file".to_string()),
+            e_type: if metadata.is_dir() {
+                EntryType::Directory
+            } else {
+                EntryType::File
+            },
+            len_byte: metadata.len(),
+            modified: "".to_string(),
+        })
+    }
+
 }
