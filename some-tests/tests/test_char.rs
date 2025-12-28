1ecedcf5375feee6a29c0f9bb6061ec1b2009601
obarra-dev <obarra@sonatype.com>
2025-12-28 16:28:41 -0300

    clean code

--- some-tests/tests/test_char.rs
+++ some-tests/tests/test_char.rs
@@ -12,3 +12,18 @@ fn size_of_val() {
     let s = std::mem::size_of_val(&c);
     assert_eq!(s, 4);
 }
+
+#[test]
+fn rev() {
+    let s = String::from("omar");
+    let chars = s.chars();
+    let rev = chars.rev();
+    let s_reverted:String = rev.collect();
+    assert_eq!(s_reverted, "ramo");
+
+    let s_reverted:String = s.chars().rev().collect();
+    assert_eq!(s_reverted, "ramo");
+
+    let s_reverted:Vec<char> = s.chars().rev().collect();
+    assert_eq!(s_reverted, vec!['r', 'a', 'm', 'o']);
+}
